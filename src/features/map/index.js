c0443c2d4b2e8f69b8350e274fb1b763aa9afb8d
Tom Dugan <thomas@aadwebb.com>
2019-07-12 11:39:24 -0500

    defined boundaries and barriers

--- src/features/map/index.js
+++ src/features/map/index.js
@@ -1,4 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { SPRITE_SIZE } from '../../config/constants';
+
+import './styles.css'
+
+function getTileSprite(type) {
+  switch(type) {
+    case 0:
+      return 'grass'
+    case 5:
+      return 'rock'
+    case 6:
+      return 'tree'
+  }
+}
+
+function MapTile(props) {
+  return <div
+    className={`tile ${getTileSprite(props.tile)}`}
+    style={{
+      height: SPRITE_SIZE,
+      width: SPRITE_SIZE,
+    }}
+     />
+}
+
+function MapRow(props) {
+  return <div className="row">
+    {
+      props.tiles.map( tile => <MapTile tile={tile} />)
+    }
+  </div>
+}
 
 function Map(props) {
   return(
@@ -12,8 +45,18 @@ function Map(props) {
         backgroundColor: 'palegreen',
         border: '4px solid white'
       }}
-    />
+    >
+      {
+        props.tiles.map( row => <MapRow tiles={row} /> )
+      }
+    </div>
   );
 }
 
-export default Map;
+function mapStateToProps(state) {
+  return {
+    tiles: state.map.tiles
+  }
+}
+
+export default connect(mapStateToProps)(Map);
